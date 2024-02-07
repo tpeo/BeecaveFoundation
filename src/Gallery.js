af671ae5325ccd9675d8ad0ad05595d6f42a1429
Riya Singla <riyasingla2021@gmail.com>
2024-02-07 16:25:48 -0600

    edited responsive gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import CardGroup from 'react-bootstrap/CardGroup';
-import BootstrapCard from './BootstrapCard'; 
-import ImageGallery from 'react-image-gallery';
-//import 'react-image-gallery/styles/css/image-gallery.css';
-import './Gallery.css';
-
+import ResponsiveMasonry from 'react-responsive-masonry';
+import './Gallery.css'; 
 
 const spreadsheetId = '14INJd2S6B9SOqxl2FnBZT1_EOp5NEe6tWvPaCsWDp0c'; 
-const ranges = '2:12'; 
+const ranges = '2:100'; 
 const apiKey = process.env.REACT_APP_API_KEY;
-console.log(apiKey);
 const ROWS= 'ROWS';
+
+const extractImageId = (imageUrl) => {
+  const parts = imageUrl.split('id=');
+  const id = parts[1];
+  return id;
+};
 
 const Gallery = () => {
   const [artworks, setArtworks] = useState([]);
 
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
         const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchGet?ranges=${ranges}&key=${apiKey}&majorDimension=${ROWS}`);
         const data = await response.json();
         if (data.valueRanges && data.valueRanges[0].values) {
-          console.log(data.valueRanges[0].values);
-          const fetchedArtworks = data.valueRanges[0].values.map((row) => ({
-            imageUrl: row[2],
+          const fetchedArtworks = data.valueRanges[0].values.map((row, index) => ({
+            imageUrl: extractImageId(row[2]),
             description: row[3],
+            imageId: extractImageId(row[2]),
+            key: index.toString(),
           }));
-          console.log(fetchedArtworks);
           setArtworks(fetchedArtworks);
-          console.log(artworks);
         } else {
           console.log('No data found.');
         }
@@ -38,13 +38,24 @@ const Gallery = () => {
     };
 
     fetchData();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="gallery-container">
-    {artworks.map((artwork, index) => (
-      <BootstrapCard key={artwork.description} {...artwork} />
-    ))}
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        {artworks.map((artwork) => (
+          <div key={artwork.key} className="gallery-item">
+            <img
+              src={`https://drive.google.com/thumbnail?id=${artwork.imageId}`}
+              alt={artwork.description}
+              className="gallery-image"
+            />
+            <div className="image-overlay">
+              {artwork.description}
+            </div>
+          </div>
+        ))}
+      </ResponsiveMasonry>
     </div>
   );
 };
