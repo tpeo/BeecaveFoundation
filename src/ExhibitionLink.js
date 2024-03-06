1c6616353db3f55c8a9c192af2afbc1e8427d44b
Emerald-Z <ejzhang2004@gmail.com>
2024-03-05 18:26:04 -0600

    added mui and formatted home and exhibitions a little

--- src/ExhibitionLink.js
+++ src/ExhibitionLink.js
@@ -1,16 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  Box,
+  CardActions,
+  Button
+} from '@mui/material'
 
 export default function ExhibitionLink({subtitle, title, date, description}) {
 
   return (
-    <div className="gallery-container">
-        <img src='' alt={`${title}_cover_img`}></img>
-        <h1> <Link to={`/Exhibition/${title}`}>{title}</Link> </h1>        <h3>{date}</h3>
-        <p>{description}</p>
+    <Card sx={{ display: 'flex', justifyContent: "space-between", paddingX: '5%', paddingY: '1%' }}>
+       <Box sx={{ display: 'flex', direction: 'column', justifyContent: "space-between", pl: 1, pb: 1 }}>
+              <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+            <CardActions>
+              <Link to={`/Exhibition/${title}`} size="small">Visit</Link>
+            </CardActions>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              {subtitle}
+            </Typography>
+            <Typography variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {date}
+            </Typography>
+            <Typography variant="body2">
+              {description}
+            </Typography>     
+          </CardContent>
+        </Box>
+       
+      </Card>
 
-    </div>
   );
 };
