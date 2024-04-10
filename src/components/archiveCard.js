4f770cb7adc0a8151a4549329bf89fa119b09bf2
Emerald-Z <ejzhang2004@gmail.com>
2024-04-09 21:54:50 -0500

    made archive clickable

--- src/components/archiveCard.js
+++ src/components/archiveCard.js
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import {
+  Button,
   Card,
   CardContent,
   Typography,
   CardMedia,
   Box,
   CardActions,
-  Grid
+  Grid,
+  ButtonBase
 } from '@mui/material'
-
+// import { Button } from 'react-bootstrap';
 export default function ArchiveCard({title, date, image}) {
 
-  
+  const navigate = useNavigate();
+
   return (
-        <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'center', paddingX: '5%', paddingY: '1%',
+
+        <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'center',
                 border: "none", boxShadow: "none"}}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: "space-between", pl: 1, pb: 1 }}>
+            <ButtonBase to={`/Exhibition/${title}`} sx={{ display: 'flex', flexDirection: 'column'}} >
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: "space-between", pl: 1, pb: 1, width: "100%" }}>
                 <CardMedia
                     component="img"
                     image={`https://drive.google.com/thumbnail?id=${image}`}
@@ -29,6 +34,7 @@ export default function ArchiveCard({title, date, image}) {
             <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 {date}
             </Typography>
+            </ButtonBase>
         </Card>
 
   );
